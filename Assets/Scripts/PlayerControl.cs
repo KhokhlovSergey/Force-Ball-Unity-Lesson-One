8e8af0e0347ec68e44dae2cc67c2560a46d7cad4
Gizze-HP\Gizze <Svetozarpk@gmail.com>
2017-12-02 14:41:54 +1000

    Added more functional and leurn how its work; I still have very bad Enflish skill, but i dont care.

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    private Rigidbody rb;
+    private float speed = 10.0f;
+
+    public void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+    public void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Pick up")) {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
